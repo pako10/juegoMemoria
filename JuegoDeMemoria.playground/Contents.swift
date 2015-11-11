d7ad2aebebdb00ac5dff4415f6bf9f4bee1f0fe9
pako10 <pakoAndrade@Eduardos-MacBook-Pro.local>
2015-11-10 19:11:33 -0600

    JuegoMemoria2.0

---
+++ JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for var i = 0; i < 101; i++ {
+    if i % 5 == 0{
+        print("\(i) + Bingo!!!")
+    }
+    if i % 2 == 0 {
+        print("\(i) + par!!!")
+    }else {
+        print("\(i) + impar!!!")
+    }
+    if i > 29 && i < 41{
+        print("\(i) + Viva Swift!!!")
+    }
+}
